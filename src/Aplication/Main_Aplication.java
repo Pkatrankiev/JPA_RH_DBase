be321d4117f17f60edacff14d179d2e57b542812
Pkatrankiev <pkatrankiev@abv.bg>
2018-01-30 22:00:01 +0200

    corect pokazatel

--- src/Aplication/Main_Aplication.java
+++ src/Aplication/Main_Aplication.java
@@ -30,8 +30,8 @@ public class Main_Aplication {
 
 		// SetBasikValueInDataBase();
 
-//		String fileName = "c:\\Users\\Acer\\Desktop\\3344.doc";
-		 String fileName = "c:\\Users\\ALPHA\\Desktop\\3150.doc";
+		String fileName = "c:\\Users\\Acer\\Desktop\\3281.doc";
+//		 String fileName = "c:\\Users\\ALPHA\\Desktop\\3150.doc";
 		 SetDBfromWordDoc.setVolume(fileName);
 
 //		 ReaderWordDoc.readMyDocument(fileName);

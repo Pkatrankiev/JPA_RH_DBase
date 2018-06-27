4a395d07c0c1d6b22950f0192bd8b23d940f7b3c
ALPHAPC\ALPHA <pkatrankiev@abv.bg>
2018-06-27 16:18:22 +0300

    tabb

--- src/table/StartReplaceTemplateWord.java
+++ src/table/StartReplaceTemplateWord.java
@@ -1,31 +1,39 @@
-package WindowViewAplication;
+package table;
+
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 
+import org.apache.log4j.BasicConfigurator;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
-public class ManipolDoc {
+import doc.GenerateWordDoc;
+
+public class StartReplaceTemplateWord {
+	
 private static WordprocessingMLPackage template = null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BasicConfigurator.configure();
+//		log.info("This is Logger Info");
 		
 		try {
-			template = GenerateWordDoc.getTemplate("E:\\123.docx");
+			template = GenerateWordDoc.getTemplate("template2.docx");
 		} catch (FileNotFoundException | Docx4JException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		GenerateWordDoc.replacePlaceholder(template,"33333333333","$$request_date$$");
-		
+		MapTable();
 		try {
-			GenerateWordDoc.writeDocxToStream(template, "D:\\temp12.docx");
+			GenerateWordDoc.writeDocxToStream(template, "temp12.docx");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,27 +42,26 @@ private static WordprocessingMLPackage template = null;
 			e.printStackTrace();
 		}
 	}
-	private void MapTable(){
+	private static void MapTable(){
+		ArrayList<Map<String,String>> listValue = new ArrayList<Map<String,String>>();
 		Map<String,String> repl1 = new HashMap<String, String>();
+		for (int i = 0; i < 40; i++) {
+		
 		repl1.put("SJ_FUNCTION", "function1");
 		repl1.put("SJ_DESC", "desc1");
 		repl1.put("SJ_PERIOD", "period1");
+		repl1.put("SJ_PERIOD3", "gamma");
 
-		Map<String,String> repl2 = new HashMap<String, String>();
-		repl2.put("SJ_FUNCTION", "function2");
-		repl2.put("SJ_DESC", "desc2");
-		repl2.put("SJ_PERIOD", "period2");
-
-		Map<String,String> repl3 = new HashMap<String, String>();
-		repl3.put("SJ_FUNCTION", "function3");
-		repl3.put("SJ_DESC", "desc3");
-		repl3.put("SJ_PERIOD", "period3");
+		listValue.add(repl1);
+		
+		}
 
 		try {
-			GenerateWordDoc.replaceTable(new String[]{"SJ_FUNCTION","SJ_DESC","SJ_PERIOD"}, Arrays.asList(repl1,repl2,repl3), template);
+			GenerateWordDoc.replaceTable(new String[]{"SJ_FUNCTION","SJ_DESC","SJ_PERIOD"}, listValue, template);
 		} catch (Docx4JException | JAXBException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		}
 }
+

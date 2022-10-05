5f71f7e09d74f6d457461e37e6426d40a9ec64ee
pkatrankiev <pkatrankiev@abv.bg>
2022-10-05 22:09:57 +0300

    new

--- src/OldClases/TestClases.java
+++ src/OldClases/TestClases.java
@@ -428,6 +428,8 @@ public	static void ChoiceListIzpPokazatel() {
 		thread.start();
 	}
 
+	
+	
 	@SuppressWarnings("unused")
 	private static void updateUserRedac() {
 		Users user = UsersDAO.getValueUsersById(3);
